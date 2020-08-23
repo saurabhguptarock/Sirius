94189bcb6d5713ea77787fb538ed290e9151b5f4
saurabh gupta <saurabhguptarock119@gmail.com>
2020-08-23 09:46:53 +0530

    .

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,5 @@
 import Header from "../Header";
 import dynamic from "next/dynamic";
-import ReactNotification from "react-notifications-component";
 
 const Footer = dynamic(() => import("../Footer"));
 
