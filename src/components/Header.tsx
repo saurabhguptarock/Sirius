ae5819ed37d3b45bbee94e403e34b45548f3f887
saurabh gupta <saurabhguptarock119@gmail.com>
2020-08-26 22:47:21 +0530

    added add icon popover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import { connect } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { login, logout } from "../store/actions/AuthAction";
 import FirebaseService from "../services";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { User } from "../types";
 import React from "react";
+import Popover from "react-tiny-popover";
+import AddIconPopover from "./AddIconPopover";
 
 interface Props {
   user?: User;
@@ -14,6 +16,9 @@ interface Props {
 
 const Header = (props: Props) => {
   const router = useRouter();
+  const [plusPopoverOpen, setPlusPopoverOpen] = useState(false);
+  const [wallpaperPopover, setWallpaperPopover] = useState(false);
+  const [notificationPopover, setNotificationPopover] = useState(false);
 
   useEffect(() => {
     FirebaseService.auth.onAuthStateChanged(async (user) => {
@@ -29,16 +34,16 @@ const Header = (props: Props) => {
       className="navbar is-transparent is-fixed-top-desktop"
       role="navigation"
       aria-label="main navigation"
+      style={{
+        backgroundColor:
+          router.pathname.split("/")[1] === "boards"
+            ? "rgba(0,0,0,0.6)"
+            : "#026AA7",
+      }}
     >
       <div className="navbar-brand">
         <Link href="/">
-          <a
-            className={
-              router.pathname.split("/")[1] === "boards"
-                ? "navbar-item main"
-                : "navbar-item"
-            }
-          >
+          <a className="navbar-item main">
             <i className="fas fa-home" style={{ fontSize: "1.3rem" }}></i>
           </a>
         </Link>
@@ -59,13 +64,7 @@ const Header = (props: Props) => {
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
           <Link href="/settings">
-            <a
-              className={
-                router.pathname.split("/")[1] === "boards"
-                  ? "navbar-item main"
-                  : "navbar-item"
-              }
-            >
+            <a className="navbar-item main">
               <i className="fas fa-cog" style={{ fontSize: "1.3rem" }}></i>
             </a>
           </Link>
@@ -74,48 +73,35 @@ const Header = (props: Props) => {
         <div className="navbar-end">
           {props.user && (
             <React.Fragment>
-              <a
-                className={
-                  router.pathname.split("/")[1] === "boards"
-                    ? "navbar-item main"
-                    : "navbar-item"
+              <Popover
+                isOpen={plusPopoverOpen}
+                position={"bottom"}
+                onClickOutside={() => setPlusPopoverOpen(!plusPopoverOpen)}
+                align={"start"}
+                content={
+                  <AddIconPopover setPlusPopoverOpen={setPlusPopoverOpen} />
                 }
               >
-                <i className="fas fa-plus" style={{ fontSize: "1.3rem" }}></i>
-              </a>
-              <a
-                className={
-                  router.pathname.split("/")[1] === "boards"
-                    ? "navbar-item main"
-                    : "navbar-item"
-                }
-              >
+                <a
+                  className="navbar-item main"
+                  onClick={() => setPlusPopoverOpen(!plusPopoverOpen)}
+                >
+                  <i className="fas fa-plus" style={{ fontSize: "1.3rem" }}></i>
+                </a>
+              </Popover>
+              <a className="navbar-item main">
                 <i className="fas fa-images" style={{ fontSize: "1.3rem" }}></i>
               </a>
-              <a
-                className={
-                  router.pathname.split("/")[1] === "boards"
-                    ? "navbar-item main"
-                    : "navbar-item"
-                }
-              >
+              <a className="navbar-item main">
                 <i className="fas fa-bell" style={{ fontSize: "1.3rem" }}></i>
               </a>
+              <Link href="/user-profile">
+                <a className="navbar-item main">
+                  <i className="fas fa-user" style={{ fontSize: "1.3rem" }}></i>
+                </a>
+              </Link>
               <a
-                className={
-                  router.pathname.split("/")[1] === "boards"
-                    ? "navbar-item main"
-                    : "navbar-item"
-                }
-              >
-                <i className="fas fa-user" style={{ fontSize: "1.3rem" }}></i>
-              </a>
-              <a
-                className={
-                  router.pathname.split("/")[1] === "boards"
-                    ? "navbar-item main"
-                    : "navbar-item"
-                }
+                className="navbar-item main"
                 onClick={async () => {
                   await FirebaseService.signOut();
                   props.dispatch(logout());
@@ -128,6 +114,7 @@ const Header = (props: Props) => {
               </a>
             </React.Fragment>
           )}
+          ;
           {!props.user && (
             <div className="navbar-item">
               <div className="buttons">
